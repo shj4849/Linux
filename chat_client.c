cdfd8d7be63e436874b2aeb6146e7722903bc263
hongjae <shj4849@naver.com>
2016-11-03 04:19:27 -0700

    first commit

---
+++ chat_client.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <pthread.h>
+
+#define BUFF_SIZE 100
+#define NAME_SIZE 20
+
+void* send_msg(void* arg);
+void* recv_msg(void* arg);
+void error(char *msg);
+
+
+char name[NAME_SIZE] = "[Default]";
+char msg[BUFF_SIZE];
+
+int main(int argc, char *argv[]){
+	int sock;
+	struct sockaddr_in serv_addr;
+	pthread_t snd_thread, rcv_thread;
+	void* thread_result;
+
+	if(argc != 4){
+		printf("Usage : %s <ip> <port> \n", argv[0]);
+		exit(1);
+	}
+
+	sprintf(name, "[%s]", argv[3]);
+
+	sock = socket(PF_INET, SOCK_STREAM, 0);
+	if(sock == -1)
+		error("socket() error\n");
+
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+	serv_addr.sin_port = htons(atoi(argv[2]));
+
+	if(connect(sock,(struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+		error("connect() error\n");
+
+	pthread_create(&snd_thread, NULL, send_msg, (void*)&sock);
+	pthread_create(&rcv_thread, NULL, recv_msg, (void*)&sock);
+	pthread_join(snd_thread, &thread_result);
+	pthread_join(rcv_thread, &thread_result);
+	
+	close(sock);
+	return 0;
+}
+
+void* send_msg(void* arg){
+	int sock =*((int*) arg);
+	char name_msg[NAME_SIZE+BUFF_SIZE];
+	while(1){
+		fgets(msg, BUFF_SIZE, stdin);
+		if(!strcmp(msg, "q\n") || !strcmp(msg,"Q\n")){
+			close(sock);
+			exit(0);
+		}
+		sprintf(name_msg, "%s %s", name, msg);
+		write(sock, name_msg, strlen(name_msg));
+	}
+	return NULL;
+}
+
+void* recv_msg(void* arg){
+	int sock =*((int*) arg);
+	char name_msg[NAME_SIZE+BUFF_SIZE];
+	int str_len;
+	while(1){
+		str_len = read(sock, name_msg, NAME_SIZE+BUFF_SIZE-1);
+		if(str_len == -1)
+			return (void*)-1;
+		name_msg[str_len]=0;
+		fputs(name_msg,stdout);
+	}
+	return NULL;
+}
+
+void error(char *msg){
+	fputs(msg,stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
---
+++ chat_server.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <pthread.h>
+
+#define BUFF_SIZE 100
+#define MAX_CLNT 10
+
+void *clnt_connect(void *arg);
+void send_msg(char *msg, int len);
+void error(char *msg);
+
+int clnt_number = 0;
+int clnt_socks[MAX_CLNT];
+pthread_mutex_t mutx;
+
+int main(int argc, char *argv[]){
+	int serv_sock;
+	int clnt_sock;
+	struct sockaddr_in serv_addr;
+	struct sockaddr_in clnt_addr;
+	int clnt_addr_size;
+	pthread_t thread;
+
+	if(argc != 2){
+		printf("Usage : %s <port>\n", argv[0]);
+		exit(1);
+	}
+
+	pthread_mutex_init(&mutx, NULL);
+	
+	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+	if(serv_sock == -1)
+		error("socket() error\n");
+
+//	memset(&serv_sock, 0, sizeof(serv_sock));
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(atoi(argv[1]));
+
+	if(bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+		error("bind() error\n");
+
+	if(listen(serv_sock, 5) == -1)
+		error("listen() error\n");
+
+	while(1){
+		clnt_addr_size = sizeof(clnt_addr);
+		clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+
+		pthread_mutex_lock(&mutx);
+
+		clnt_socks[clnt_number++] = clnt_sock;
+		pthread_mutex_unlock(&mutx);
+
+		pthread_create(&thread, NULL, clnt_connect, (void*)&clnt_sock);
+		pthread_detach(thread);
+		printf("IP : %s \n",inet_ntoa(clnt_addr.sin_addr));
+	}
+	close(serv_sock);
+	return 0;
+}
+
+void *clnt_connect(void *arg){
+	int clnt_sock = *((int*) arg);
+	int str_len = 0;
+	char msg[BUFF_SIZE];
+	int i;
+
+	while((str_len = read(clnt_sock, msg, sizeof(msg))) != 0)
+		send_msg(msg, str_len);
+
+	pthread_mutex_lock(&mutx);
+	for(i=0;i<clnt_number;i++){
+		if(clnt_sock == clnt_socks[i]){
+			while(i++<clnt_number-1)
+				clnt_socks[i] = clnt_socks[i+1];
+			
+			break;
+		}
+	}
+	clnt_number--;
+	pthread_mutex_unlock(&mutx);
+	close(clnt_sock);
+	return NULL;
+}
+
+void send_msg(char *msg, int len){
+	int i;
+	pthread_mutex_lock(&mutx);
+
+	for(i=0;i<clnt_number;i++)
+		write(clnt_socks[i], msg, len);
+
+	pthread_mutex_unlock(&mutx);
+}
+
+void error(char *msg){
+	fputs(msg, stderr);
+	fputc('\n',stderr);
+	exit(1);
+}
